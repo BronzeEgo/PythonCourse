fa39329b822c6c9b1e5cbf2c649e516ec839d8aa
BronzeEgo <kkoch@bronzeegoenterprisesinc.com>
2018-09-12 07:30:02 -0400

    half way through modules. Also, VI, updated the vscode interpreter to a specific env that is not the same on both machines. Not sure how that will affect things.

---
+++ modules.py
@@ -0,0 +1,59 @@
+#Section 22 - Modules
+#Modules are nice because I can borrow hundreds (if not thousands) of prewritten modules to work with what I need to work with.
+# 1) make HTTP request
+# 2) Connect to a database
+# 3) Make a web app/game
+#What I will learn (Objectives)
+# Define what a module is
+# Import code from built-in modules
+# Import code from other files
+# Import code from external modules using pip
+# Describe common module patterns
+# 'And then we will shift our focus to' Describing the request/response cycle in HTTP
+# 'And how we can' Use the requests module to make requests to web apps
+
+#215 - Working With Built-In Modules
+#Why do I care about them?
+#Keep Python Files Small. The idea is a lot like using functions. Separate things to make it easier to read what is actually happening.
+#Reuse code across multiple files by importing. Likely the best reason to use them
+#'Module' is a fancy way of saying 'another Python file'. I have used them with Scrapy...and in this course for that matter
+
+#built-In Modules
+#built-in modules are modules that come with Python by default.
+#I still need to import them to use them (since having a bunch of useless stuff in each program would slow things down)
+#Think pdb or random, etc.
+#A comprehensive list of built-in Python modules: https://docs.python.org/3/py-modindex.html
+import random
+
+print(random.choice(['apple', 'banana', 'cherry', 'durian'])) #randomly picks one
+print(random.shuffle(['apple', 'banana', 'cherry', 'durian'])) #randomly shuffles list
+
+#if I wanted to refer to the module as another name
+
+import random as rand #I can now call on the random module using 'rand' instead of 'random'
+
+print(rand.choice(['apple', 'banana', 'cherry', 'durian'])) #randomly picks one
+print(rand.shuffle(['apple', 'banana', 'cherry', 'durian'])) #randomly shuffles list
+
+#If I just want to import A PART of a module, I can use keyword 'from'
+#It's best to only import what I need to be safe and keep things fast
+#from MODULE import * (this imports everything into the current namespace (meaning you dont need random.randint(), just randint()))
+from random import choice as c, randint as r
+
+print(c(['a', 'b', 'c', 'd']))
+print(r(1,100))
+
+#218 - Custom Modules
+# Less impressive than it sounds...basically just a file with Python code that I import to another file
+# See the lecture for details
+# I is the same as importing a built-in module
+#just make sure the files are in the same directory (or you need to specify directory location)
+
+#220 - Installing External Modules and TermColor
+#external modules need to be installed and, generally speaking, come from random developers
+#A good resource for external modules: https://pypi.org/
+#IMPORTANT NOTE: PyPi is not audited. This means I must be extremely careful with the packages I download. Most popular ones will be scrutinized, but others will likely not be...
+#to get external packages, I need to use pip in regular Python, but since I'm using Anaconda, I need the cheatsheet: 
+# https://conda.io/docs/_downloads/conda-cheatsheet.pdf
+# It's important to note that I should use different environments as I am testing things out...
+
---
+++ modules_colors.py
@@ -0,0 +1,10 @@
+#This is a note on ANSI for Windows. If I want any cool effects in the prompt, I need to download and import 'colorama' to the env I'm using.
+#Colorama is the shit
+#I may actually have use for termcolor when dealing with error handling. If I can force specific colors/effects on certain things, they will jump out at me better.
+
+from termcolor import colored
+from colorama import init
+init()
+
+text = colored("HI THERE!", color="magenta", on_color="on_cyan", attrs=["blink"])
+print(text)
